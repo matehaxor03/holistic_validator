ff4123c702f8aa47896a729f18f86f48e3f699ea
matehaxor03 <matehaxor03>
2024-01-05 10:01:55 +1100

    fix base64 validation

--- validation_constants/Base64Encoding.go
+++ validation_constants/Base64Encoding.go
@@ -38,6 +38,34 @@ func GetValidBase64EncodingCharacters() map[string]interface{} {
 	valid_chars["x"] = nil
 	valid_chars["y"] = nil
 	valid_chars["z"] = nil
+	valid_chars["A"] = nil
+	valid_chars["B"] = nil
+	valid_chars["C"] = nil
+	valid_chars["D"] = nil
+	valid_chars["E"] = nil
+	valid_chars["F"] = nil
+	valid_chars["G"] = nil
+	valid_chars["H"] = nil
+	valid_chars["I"] = nil
+	valid_chars["J"] = nil
+	valid_chars["K"] = nil
+	valid_chars["L"] = nil
+	valid_chars["M"] = nil
+	valid_chars["N"] = nil
+	valid_chars["O"] = nil
+	valid_chars["P"] = nil
+	valid_chars["Q"] = nil
+	valid_chars["R"] = nil
+	valid_chars["S"] = nil
+	valid_chars["T"] = nil
+	valid_chars["U"] = nil
+	valid_chars["V"] = nil
+	valid_chars["W"] = nil
+	valid_chars["X"] = nil
+	valid_chars["Y"] = nil
+	valid_chars["Z"] = nil
+	valid_chars["+"] = nil
+	valid_chars["/"] = nil
 	valid_chars["="] = nil
 	return valid_chars
 }
